413dfcb110449d5e455a3319af0aa76ac3b17739
Tejas Falke <tejasdf.77@gmail.com>
2023-05-29 18:33:00 +0530

    sign out

--- authentication/views.py
+++ authentication/views.py
@@ -3,12 +3,14 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+
 
 # Create your views here.
 def home(request):
     # return HttpResponse("Hello I am working ")
-    return render(request,"authentication/index.html")
+    return render(request, "authentication/index.html")
+
 
 def signup(request):
     if request.method == "POST":
@@ -23,11 +25,12 @@ def signup(request):
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
         myuser.last_name = lname
-        
+
         myuser.save()
-        messages.success(request,"Your Account Has been successfully created.")
+        messages.success(request, "Your Account Has been successfully created.")
         return redirect('signin')
-    return render(request,"authentication/signup.html")
+    return render(request, "authentication/signup.html")
+
 
 def signin(request):
     if request.method == 'POST':
@@ -38,12 +41,15 @@ def signin(request):
         if user is not None:
             login(request, user)
             fname = user.first_name
-            return render(request,"authentication/index.html", {'fname':fname})
+            return render(request, "authentication/index.html", {'fname': fname})
         else:
             messages.error(request, "Bad Credentials!")
             return redirect('home')
 
-    return render(request,"authentication/signin.html")
+    return render(request, "authentication/signin.html")
+
 
 def signout(request):
-    pass
+    logout(request)
+    messages.success(request, "Logged out successfully!")
+    return redirect('home')
